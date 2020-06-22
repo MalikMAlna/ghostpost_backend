d37e6127f4ecb4634c483d472528b829e065b3d3
Mike A <jimalik2525@gmail.com>
2020-06-22 17:04:56 -0400

    Added extra actions for viewset for post list

--- api/serializers.py
+++ api/serializers.py
@@ -1,10 +1,14 @@
 from rest_framework.serializers import ModelSerializer
-
+from rest_framework import serializers
 from ghostpost.models import Post
 
+
 class PostSerializer(ModelSerializer):
+    total_vote = serializers.SerializerMethodField()
+
+    def get_total_vote(self, obj):
+        return obj.up_vote - obj.down_vote
+
     class Meta:
         model = Post
         fields = ('id', 'b_or_r', 'content', 'up_vote', 'down_vote', 'created', 'total_vote')
-
-
--- api/views.py
+++ api/views.py
@@ -1,4 +1,6 @@
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from api.serializers import PostSerializer
 from ghostpost.models import Post
@@ -8,3 +10,21 @@ class PostViewSet(ModelViewSet):
     serializer_class = PostSerializer
     basename = 'post'
     queryset = Post.objects.all()
+
+    # @action(detail=False)
+    # def highest_rated(self, request):
+    #     highest_rated = Post.objects.all().order_by('total_vote')
+    #     serializer = self.get_serializer(highest_rated, many=True)
+    #     return Response(serializer.data)
+
+    @action(detail=False)
+    def boast(self, request):
+        boast = Post.objects.filter(b_or_r=True)
+        serializer = self.get_serializer(boast, many=True)
+        return Response(serializer.data)
+
+    @action(detail=False)
+    def roast(self, request):
+        roast = Post.objects.filter(b_or_r=False)
+        serializer = self.get_serializer(roast, many=True)
+        return Response(serializer.data)
